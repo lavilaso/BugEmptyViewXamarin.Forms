c9fd24c9fcba94971081a6329a439a92a7346210
Luis Avila <avilaluis0@hotmail.com>
2020-03-03 00:58:51 -0500

    Add code

---
+++ BugEmptyViewXamarin.Forms/BugEmptyViewXamarin.Forms/CollectionViewPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BugEmptyViewXamarin.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CollectionViewPage : ContentPage
+    {
+        public CollectionViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BugEmptyViewXamarin.Forms/BugEmptyViewXamarin.Forms/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BugEmptyViewXamarin.Forms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button1_Clicked(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new CarouselViewPage());
+        }
+
+        private async void Button2_Clicked(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new CollectionViewPage());
+        }
+    }
+}
